edfb2d20c20ad017df531e099f784bde45f6f7e4
estherpunter <estherpunter5@hotmail.com>
2023-09-19 19:53:46 +0200

    huiswerkopdracht voltooid

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import Home from './pages/home/Home.jsx';
 import Appointments from './pages/appointments/Appointments.jsx';
 import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
     return (
         <>
-            <Navigation />
-            <Home />
-            <Whitening />
-            <Appointments />
-            <Cavities />
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/tanden-bleken" element={<Whitening/>}></Route>
+                <Route path="/afspraken" element={<Appointments/>}></Route>
+                <Route path="/gaatjes" element={<Cavities/>}></Route>
+            </Routes>
         </>
     );
 }
